7938b3bd471c66436e3c885018095ceb5856ba28
Godadhati Vamshi <godadhatishankar@gmail.com>
2026-02-26 10:48:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,23 +5,46 @@ const path = require("path");
 
 const app = express();
 app.use(cors());
+app.use(express.json()); // 🔴 REQUIRED to add movies
 
-// Load movies.json
 const moviesPath = path.join(__dirname, "movies.json");
-const movies = JSON.parse(fs.readFileSync(moviesPath, "utf-8"));
+
+// Helper function to read movies fresh every time
+function readMovies() {
+  return JSON.parse(fs.readFileSync(moviesPath, "utf-8"));
+}
 
 // Test route
 app.get("/", (req, res) => {
   res.send("OTT Backend Running");
 });
 
-// Movies API
+// GET movies
 app.get("/movies", (req, res) => {
+  const movies = readMovies();
   res.json(movies);
 });
 
-// Start server
+// ADD movie (NEW 🔥)
+app.post("/movies", (req, res) => {
+  const movies = readMovies();
+
+  const newMovie = {
+    id: movies.length + 1,
+    title: req.body.title,
+    thumbnail: req.body.thumbnail,
+    video: req.body.video
+  };
+
+  movies.push(newMovie);
+
+  fs.writeFileSync(moviesPath, JSON.stringify(movies, null, 2));
+
+  res.status(201).json(newMovie);
+});
+
+// Start server (INTERNET SAFE)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Backend running at http://localhost:" + PORT);
+  console.log("Backend running on port " + PORT);
 });
